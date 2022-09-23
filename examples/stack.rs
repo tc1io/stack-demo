834980265a703708fb58f7934667a8fa44dac7b9
Anand Nair <rajeevanandnair@gmail.com>
2022-09-23 15:55:29 +0530

    trying without Box

--- examples/stack.rs
+++ examples/stack.rs
@@ -1,120 +1,168 @@
-#![allow(unused)]
+// #![allow(unused)]
+//
+// struct Stack<T> {
+//     data: Link<T>,
+//     // index: T,
+//     // next: Link <T>,
+// }
+//
+// // type Link<T> = [T; 500];
+//
+// type Link<T> = Option<Box<Node<T>>>;
+//
+// // type Link<T> = Option<[T]>;
+// // Option<Node<T>>;
+//
+// struct Node<T> {
+//     elem: T,
+//     index: Link<T>,
+// }
+//
+// impl<T> Stack<T> {
+//     pub fn new() -> Self {
+//         Stack { data: None }
+//     }
+//
+//     fn push(&mut self, item: T) {
+//         let new_node = Box::new(Node {
+//             elem: item,
+//             index: self.data.take(),
+//         });
+//         self.data = Some(new_node);
+//         // self.data[&self.] = item;
+//         // self.data = &self.data + 1
+//     }
+//
+//     //CHECK WITH JAN IF RUST HELPS WITH REMOVING FIXED SIZED ARRAY
+//
+//     fn pop(&mut self) -> Option<T> {
+//         self.data.take().map(|node| {
+//             self.data = node.index;
+//             node.elem
+//         })
+//     }
+//
+//     fn peek(&self) -> Option<&T> {
+//         self.data.as_ref().map(|node|{
+//             &node.elem
+//         })
+//     }
+//
+//     fn is_empty(&self) -> bool {
+//         self.data.is_none()
+//     }
+//
+//     // fn length(&self) -> Link<T> {
+//     //     self.data
+//     // }
+//
+//     // fn check() {
+//     //     for i in 1..1000 {
+//     //         println!("The values are {:?}", i);
+//     //     }
+//     // }
+// }
+//
+// #[cfg(test)]
+//
+// mod tests {
+//     use super::*;
+//
+//     #[test]
+//     fn test_push() {
+//         let mut stack = Stack::new();
+//
+//         assert_eq!(stack.pop(), None);
+//
+//         stack.push(1);
+//         stack.push(2);
+//         stack.push(3);
+//
+//         assert_eq!(stack.pop(), Some(3));
+//         assert_eq!(stack.pop(), Some(2));
+//
+//         stack.push(4);
+//         stack.push(5);
+//
+//         assert_eq!(stack.peek(), Some(&5))
+//     }
+//     //
+//     // #[test]
+//     // fn test_pop() {
+//     //     let mut stack = StackI32::new_data();
+//     //     stack.push(10);
+//     //     stack.push(11);
+//     //     stack.push(1);
+//     //     stack.pop();
+//     //     assert_eq!(stack.length(), 2);
+//     //     stack.pop();
+//     //     assert_eq!(stack.length(), 1);
+//     //     stack.pop();
+//     //     assert_eq!(stack.length(), 0);
+//     // }
+//     //
+//     // #[test]
+//     // fn test_len() {
+//     //     let mut stack = StackI32::new_data();
+//     //     stack.push(1);
+//     //     stack.push(10);
+//     //     assert_eq!(2, stack.length());
+//     // }
+//
+//     // #[test]
+//     // fn test_peek() {
+//     //     let mut stack = StackI32::new_data();
+//     //     stack.push(1);
+//     //     stack.push(10);
+//     //     assert_eq!(stack.peek(), Some(&2))
+//     // }
+// }
 
 struct Stack<T> {
-    data: Link<T>,
-    // index: T,
-    // next: Link <T>,
+    data: [T; 500],
+    index: T,
 }
-
-// type Link<T> = [T; 500];
-
-type Link<T> = Option<Box<Node<T>>>;
-
-// type Link<T> = Option<[T]>;
-// Option<Node<T>>;
-
-struct Node<T> {
-    elem: T,
-    index: Link<T>,
-}
-
 impl<T> Stack<T> {
-    pub fn new() -> Self {
-        Stack { data: None }
+
+    fn new(data: [T; 500], index: T) -> Self {
+        Self { data, index }
     }
 
     fn push(&mut self, item: T) {
-        let new_node = Box::new(Node {
-            elem: item,
-            index: self.data.take(),
-        });
-        self.data = Some(new_node);
-        // self.data[&self.] = item;
-        // self.data = &self.data + 1
+        self.data[&self.index] = item;
+        self.index = &self.index + 1
     }
 
-    //CHECK WITH JAN IF RUST HELPS WITH REMOVING FIXED SIZED ARRAY
-
-    fn pop(&mut self) -> Option<T> {
-        self.data.take().map(|node| {
-            self.data = node.index;
-            node.elem
-        })
+    fn pop(&mut self) -> T {
+        let val = self.data[&self.index];
+        self.index = &self.index - 1;
+        return val
     }
 
     fn peek(&self) -> Option<&T> {
-        self.data.as_ref().map(|node|{
-            &node.elem
-        })
+        if !self.is_empty() {
+            Some(&self.index)
+        } else {
+            None
+        }
     }
 
     fn is_empty(&self) -> bool {
-        self.data.is_none()
+        if self.index == 0 {
+            true
+        } else {
+            false
+        }
     }
 
-    // fn length(&self) -> Link<T> {
-    //     self.data
-    // }
+    fn length(&self) -> T {
+        &self.index
+    }
 
     // fn check() {
     //     for i in 1..1000 {
     //         println!("The values are {:?}", i);
     //     }
-    // }
-}
-
-#[cfg(test)]
-
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_push() {
-        let mut stack = Stack::new();
-
-        assert_eq!(stack.pop(), None);
-
-        stack.push(1);
-        stack.push(2);
-        stack.push(3);
-
-        assert_eq!(stack.pop(), Some(3));
-        assert_eq!(stack.pop(), Some(2));
-
-        stack.push(4);
-        stack.push(5);
-
-        assert_eq!(stack.peek(), Some(&5))
-    }
-    //
-    // #[test]
-    // fn test_pop() {
-    //     let mut stack = StackI32::new_data();
-    //     stack.push(10);
-    //     stack.push(11);
-    //     stack.push(1);
-    //     stack.pop();
-    //     assert_eq!(stack.length(), 2);
-    //     stack.pop();
-    //     assert_eq!(stack.length(), 1);
-    //     stack.pop();
-    //     assert_eq!(stack.length(), 0);
-    // }
-    //
-    // #[test]
-    // fn test_len() {
-    //     let mut stack = StackI32::new_data();
-    //     stack.push(1);
-    //     stack.push(10);
-    //     assert_eq!(2, stack.length());
-    // }
-
-    // #[test]
-    // fn test_peek() {
-    //     let mut stack = StackI32::new_data();
-    //     stack.push(1);
-    //     stack.push(10);
-    //     assert_eq!(stack.peek(), Some(&2))
     // }
 }
 
@@ -124,4 +172,18 @@ fn main() {
     //         println!("The values are {:?}", i);
     //     }
     // }
+
+    let mut stack = Stack::new();
+    stack.push(1);
+    stack.push(2);
+    stack.push(3);
+    stack.pop();
+    stack.pop();
+    stack.push(4);
+    stack.push(5);
+    stack.push(6);
+    stack.push(7);
+
+    println!("The stack length is {:?}", stack.length());
+    println!("The last added value is {:?}", stack.peek());
 }
